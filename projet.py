005ce6dc3715336715c2296bb5883409b86f87d0
EJ-ipsien <60393618+EJ-ipsien@users.noreply.github.com>
2020-02-05 20:50:47 +0100

    Add files via upload

--- projet.py
+++ projet.py
@@ -2,8 +2,17 @@ import pygame
 pygame.init()
 
 pygame.display.set_caption("projet terminal")
-pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((800,400))
+
+background = pygame.image.load('projet term/castle.png')
 
 running = True
 while running:
 
+    screen.blit(background,(0, 0))
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
