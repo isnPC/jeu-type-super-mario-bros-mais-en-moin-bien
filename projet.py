88e1803c42fb13a100b3e1d9e8e8a26ebc253fef
EJ-ipsien <60393618+EJ-ipsien@users.noreply.github.com>
2020-02-05 22:00:53 +0100

    Add files via upload

--- projet.py
+++ projet.py
@@ -2,14 +2,14 @@ import pygame
 pygame.init()
 
 pygame.display.set_caption("projet terminal")
-screen = pygame.display.set_mode((800,1200))
+screen = pygame.display.set_mode((400,600))
 
-background = pygame.image.load('projet term/background.png')
+background = pygame.image.load('projet term/backgroundpluspetit.png')
 
 running = True
 while running:
 
-    screen.blit(background,(0,0 ))
+    screen.blit(background,(-10,-198 ))
     pygame.display.flip()
 
     for event in pygame.event.get():
