139fc1d09493ad527c2fee63ed3d1fff4f6df48e
EJ-ipsien <60393618+EJ-ipsien@users.noreply.github.com>
2020-02-06 18:44:38 +0100

    Add files via upload

--- projet.py
+++ projet.py
@@ -1,15 +1,31 @@
 import pygame
 pygame.init()
 
+class player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 15
+        self.velocity = 5
+        self.image = pygame.image.load('projet term/hero detouré2.png')
+        self.rect = self.image.get_rect()
+
 pygame.display.set_caption("projet terminal")
 screen = pygame.display.set_mode((400,600))
 
 background = pygame.image.load('projet term/backgroundpluspetit.png')
 
+player = player()
+
 running = True
 while running:
 
     screen.blit(background,(-10,-198 ))
+
+    screen.blit(player.image,player.rect)
+
     pygame.display.flip()
 
     for event in pygame.event.get():
